8b0bf2dfddae407ef6a56f55aa0834ba5ff7bdfa
celarim <k6jun@kookmin.ac.kr>
2025-04-01 16:51:12 +0900

    [Init] vue page 작성
    요구사항에 정의된 페이지들 작성

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+import Navbar from './shop/common/component/Navbar.vue';
+import AdminNavbar from "./admin/common/component/AdminNavbar.vue";
+import {useRoute} from "vue-router";
+import AdminFooter from "./admin/common/component/AdminFooter.vue";
+
+const route = useRoute();
 </script>
 
 <template>
+  <AdminNavbar v-if="route.path.includes('Admin')"></AdminNavbar>
+  <Navbar v-else></Navbar>
   <router-view></router-view>
+  <AdminFooter v-if="route.path.includes('Admin')"></AdminFooter>
+  <Footer v-else></Footer>
 </template>
 
 <style scoped>
