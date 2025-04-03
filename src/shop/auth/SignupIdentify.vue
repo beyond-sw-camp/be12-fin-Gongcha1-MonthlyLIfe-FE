815f8ef570a4a2daa7de6228a943398af3372b7f
celarim <k6jun@kookmin.ac.kr>
2025-04-03 12:20:20 +0900

    [Feat] 회원가입 페이지 작성
    1. 본인인증 페이지
    2. 회원정보 입력 페이지
    3. 회원가입 완료 페이지
    1->2->3으로 넘어가게 link 추가
    기본 양식 작성, 1, 3은 단순해서 추후 변경가능성 높음

--- src/shop/auth/SignupIdentify.vue
+++ src/shop/auth/SignupIdentify.vue
@@ -4,6 +4,22 @@
 
 <template>
 
+  <div class="container">
+    <div class="d-flex" style="width: 50vh;">
+      <p class="fs-2">본인인증하기</p>
+    </div>
+    <div class="mt-5 mb-5">
+      <p>Monthly Life는 1인 가구를 위한 월정액 서비스 입니다.</p>
+      <p>원활한 대여를 위해 본인인증을 진행해주시기 바랍니다.</p>
+    </div>
+
+    <div class="mt-5 mb-5">
+      <button>휴대폰 인증하기</button>
+    </div>
+    <router-link to="/auth/signup/input" class="btn btn-danger rounded-pill w-100">
+      다음
+    </router-link>
+  </div>
 </template>
 
 <style scoped>
--- src/shop/auth/SignupInput.vue
+++ src/shop/auth/SignupInput.vue
@@ -3,9 +3,90 @@
 </script>
 
 <template>
+  <div class="container">
+    <div class="d-flex" style="width: 50vh;">
+      <p class="fs-2">회원정보입력</p>
+    </div>
 
+    <small class="text-start d-block">MonthlyLife 가입을 위해 정보를 입력해 주세요.</small>
+
+    <div class="pt-3 pb-3">
+      <small class="text-start d-block">휴대폰번호</small>
+      <input type="number" class="form-control" :disabled="true" placeholder="01012345678">
+    </div>
+
+
+    <div class="pb-3">
+      <small class="text-start d-block">아이디</small>
+      <div class="d-flex">
+        <input type="text" class="underline-input">
+        <button class="btn btn-danger text-nowrap btn-sm" style="font-size: 10px">중복체크</button>
+      </div>
+    </div>
+
+    <div class="pb-3 d-flex justify-content-between">
+      <small class="text-start d-block">소셜로그인 연동</small>
+      <div class="d-flex">
+        <div class="p-1">Naver</div>
+        <div class="p-1">Kakao</div>
+        <div class="p-1">Google</div>
+
+      </div>
+    </div>
+
+    <div class="pb-3">
+      <small class="text-start d-block">비밀번호</small>
+        <input type="password" class="underline-input">
+    </div>
+
+    <div class="pb-3">
+      <small class="text-start d-block">비밀번호확인</small>
+      <input type="password" class="underline-input">
+    </div>
+
+    <div class="pb-3">
+      <small class="text-start d-block">주소</small>
+      <div class="d-flex">
+        <input type="password" class="underline-input">
+        <button class="btn btn-dark text-nowrap btn-sm" style="font-size: 10px">검색</button>
+      </div>
+    </div>
+    <div class="pb-3">
+      <small class="text-start d-block">상세주소</small>
+      <input type="password" class="underline-input">
+    </div>
+
+    <small class="text-start d-block">태그</small>
+    <div class="m-3">
+      <div class="form-check text-start">
+        <input class="form-check-input limit-check" type="checkbox" id="check1">
+        <label class="form-check-label" for="check1">1인가구</label>
+      </div>
+      <div class="form-check text-start">
+        <input class="form-check-input limit-check" type="checkbox" id="check2">
+        <label class="form-check-label" for="check1">신혼부부</label>
+      </div>
+      <div class="form-check text-start">
+        <input class="form-check-input limit-check" type="checkbox" id="check3">
+        <label class="form-check-label" for="check1">1인 사무실</label>
+      </div>
+    </div>
+
+
+
+    <router-link to="/auth/signup/end" class="btn btn-danger rounded-pill w-100">다음</router-link>
+
+  </div>
 </template>
 
 <style scoped>
-
+.underline-input {
+  border: none;
+  border-bottom: 1px solid #000; /* 밑줄 스타일 */
+  outline: none; /* 클릭 시 파란 테두리 제거 */
+  width: 100%; /* 입력창 너비 조절 */
+  padding: 5px 0; /* 위아래 여백 조절 */
+  background: transparent; /* 배경 제거 */
+  font-size: 16px;
+}
 </style>
