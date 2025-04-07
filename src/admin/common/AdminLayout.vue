c837cf9a2745aa313f4b143aca994e91916ed749
= <sjhyun0926@gmail.com>
2025-04-07 15:51:05 +0900

    [Feat] 관리자 페이지 구현 - 라우터 링크 달기 - 재고 관리 페이지

--- src/admin/common/AdminLayout.vue
+++ src/admin/common/AdminLayout.vue
@@ -1,20 +1,25 @@
 <script setup>
 import AdminNavbar from "./component/AdminNavbar.vue";
 import AdminFooter from "./component/AdminFooter.vue";
+import AdminHeader from "./component/AdminHeader.vue";
 </script>
 
 <template>
-  <div class="d-flex min-vh-100 gap-0">
-    <!-- 왼쪽 사이드바 -->
+  <div class="layout-container d-flex min-vh-100">
     <AdminNavbar />
-
-    <!-- 오른쪽 콘텐츠 영역 -->
+    <!-- 여기에 d-flex 추가 -->
+    <div class="d-flex flex-column flex-grow-1">
+      <AdminHeader />
       <router-view class="flex-grow-1" />
       <AdminFooter />
     </div>
+  </div>
 </template>
 
 
 <style scoped>
-/* 필요 시 추가적인 스타일 가능 */
+.layout-container {
+  margin: 0;
+  padding: 0;
+}
 </style>
---
+++ src/admin/common/component/AdminHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="custom-nav bg-white border-bottom w-100">
+    <div class="container d-flex justify-content-between align-items-center py-3">
+      <!-- 메뉴 리스트 -->
+      <ul class="nav flex-row mb-0 flex-nowrap text-nowrap">
+        <li class="nav-item">
+          <router-link to="/admin" class="nav-link text-dark fw-semibold custom-nav-link">
+            Monthly Life Admin
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
